7faeb63860490150c1732becd9ace985ffa65f08
ErikHenderson-dev <erikhenderson.dev@gmail.com>
2021-06-10 09:51:24 -0300

    Configurado project root e static root do projeto, finalizando as configurações da api com mulpas conexões.

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -139,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.getenv('STATIC_ROOT',os.path.join(PROJECT_ROOT,'static_root'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
